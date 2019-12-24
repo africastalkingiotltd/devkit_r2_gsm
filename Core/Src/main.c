7728528d98b314457098a7139b92182b4ae23e42
Kennedy Otieno <otienoken@outlook.com>
2019-12-24 15:13:25 +0300

    feat
    Finish core GSM connectivity logic

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -30,7 +30,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "../../Systick/gsm_systick.h"
+#include "../../GSM/gsm.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -50,7 +51,21 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+uint8_t apn_netw[30] = {"safaricom"}; 
+uint8_t apn_user[10] = {"saf"}; 
+uint8_t apn_pass[10] = {"data"};
+GSMModuleState gsmModuleState;
+int sent_failed_count                  = 0; 
+uint8_t tcp_connection_failed_count    = 0;
+uint32_t sim_disconnected              = 0;
+uint8_t command_failed_count           = 0;
+int sim_module_on_counter              = 0;
+// int network_registration_failure_count = 0;
+// int gsm_command_failure_count          = 0;
+// int gprs_registration_failed_count     = 0;
 
+const char *test_endpoint = "broker.africastalking.com";
+int test_port             = 1883;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -96,7 +111,11 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  UART1FIFOInit();
+  UART2FIFOInit();
+  serialPrint("Starting up... \r\n");
+  uint8_t data_length = 0;
+  uint8_t data_bucket[100];
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -104,7 +123,74 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    if (gsmModuleState == Off)
+    {
+      serialPrint("SIM State is Off. Attempting to Initialize \r\n");
+      if (initializeSIMModule())
+      {
+        gsmModuleState = On;
+        serialPrint("SIM Module initilized successfully \r\n");
+      }
+    }
+    if (gsmModuleState == On)
+    {
+      serialPrint("SIM module is on \r\n");
+      sim_module_on_counter++;
+      if (sim_module_on_counter > 300)
+      {
+        // Should we reset the SIM Module after 300 cycles?
+        serialPrint("Reset ? \r\n");
+      }
+      if (checkSIMNetworkState())
+      {
+        if (setupTCP())
+        {
+          gsmModuleState = Registered;
+          serialPrint("Network registration successfull \r\n");
+        }
+      }      
+    }
+    if (sent_failed_count > 10)
+    {
+      gsmModuleState              = On;
+      sent_failed_count           = 0;
+      tcp_connection_failed_count = 0;
+      serialPrint("GPRS Connection failed \r\n");
+    }
+    if (tcp_connection_failed_count > 20)
+    {
+      gsmModuleState              = On;
+      tcp_connection_failed_count = 0;
+      sent_failed_count           = 0;
+      serialPrint("TCP Connection failed \r\n");
+    }
+    if (gsmModuleState == Registered)
+    {
+      gsmKeepAlive();
+      getTCPStatus(300);
+      if (tcpConnectionObject.state == CONNECTED)
+      {
+        serialPrint("Connected to TCP Endpoint. Nothing to do. \r\n");
+      }else if((tcpConnectionObject.state == CLOSED) || (tcpConnectionObject.state == INITIAL))
+      {
+        data_length = snprintf((char *)data_bucket, 64, "AT+CIPSTART=0,\"TCP\",\"%s\",\"%i\"\r",test_endpoint, test_port);
+        if (sendATCommand(data_bucket, data_length, "CONNECT OK", 300))
+        {
+          serialPrint("Connection to endpoint done successfully \r\n");
+        }else 
+        {
+          serialPrint("Unable to establish connection to endpoint \r\n");
+          sent_failed_count++;
+        }
+      }
+    }
+    if (tcpConnectionObject.state == CONNECTED)
+    {
+      serialPrint("Still connected...Nothing to do \r\n");
+    }else
+    {
+      serialPrint("Disconnected ...\r\n");
+    }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -150,7 +236,7 @@ void SystemClock_Config(void)
 /* USER CODE BEGIN 4 */
 int serialPrint(const char *string_format, ...)
 {
-  if (DEBUG)
+  if (DEVEL)
   {
     return printf(string_format);
   }
